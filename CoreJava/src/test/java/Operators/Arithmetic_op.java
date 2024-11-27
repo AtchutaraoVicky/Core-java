a1d5a94f7c32786ceda7471f832ddd454d9ffead
Devicky125143 <devicky125@gmail.com>
2024-11-27 11:25:31 +0530

    Operators adding

---
+++ CoreJava/src/test/java/Operators/Arithmetic_op.java
@@ -0,0 +1,17 @@
+package Operators;
+
+ // Arithmetic operators is used to perform the mathmetical caluculatin on numarical datatypes like int , float, double
+
+public class Arithmetic_op {
+  
+   public static void main(String[] args) {
+	 int a=10;
+	  int b=40;
+	  
+	  int addition =a+b;
+	  System.out.println("Addition : "+addition);
+	  
+	  int subscription=b-a;
+	  System.out.println("Subscription : "+subscription);
+}
+}
